73ff38223d670764c6c204987070c81f3650e406
Leandro Martins <leandro.martins.85@gmail.com>
2022-11-15 16:14:35 -0300

    Box shadow menu effect

---
+++ assets/js/main/scrollHeader.js
@@ -0,0 +1,7 @@
+/*= ============== CHANGE BACKGROUND HEADER =============== */
+function scrollHeader() {
+  const nav = document.getElementById('header');
+  // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
